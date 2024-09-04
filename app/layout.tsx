e8235ed862d55941f2b2efae6dc55fdc31d4b188
Sreerag <srrgsreekanth001@gmail.com>
2024-09-04 15:52:42 +0530

    Fix conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,6 @@ export default function RootLayout({ children, }: { children: React.ReactNode; }
             <body className={inter.className}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
                     <Navbar />
-
                     {children}
                     <Footer />
                     
